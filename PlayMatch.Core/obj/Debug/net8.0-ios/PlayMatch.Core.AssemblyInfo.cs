e6baba90ebb95cf3c6e190fd2f712780a57a37ef
Henrique Ameixa <henrique.ameixa@prosperi.com.br>
2025-02-27 07:54:39 -0300

    Atualiza dependências e configurações do projeto
    Vários arquivos binários foram alterados, incluindo `CodeChunks.db` e `SemanticSymbols.db`, refletindo modificações significativas nos dados e na configuração do projeto.
    
    A versão informacional da montagem em `PlayMatch.Core.AssemblyInfo.cs` foi atualizada, assim como as plataformas de destino para `Android34.0`, `iOS18.0`, `MacCatalyst18.0` e `Windows10.0.19041.0`.
    
    O caminho do diretório do projeto foi alterado para um novo usuário, e os arquivos de configuração do NuGet foram atualizados para refletir novos caminhos e versões de pacotes, incluindo a atualização do `NuGetToolVersion` para `6.13.2` e do `runtimeIdentifierGraphPath` para `9.0.200`.
    
    O hash do `dgSpec` foi modificado, indicando mudanças significativas nas dependências do projeto.

--- PlayMatch.Core/obj/Debug/net8.0-ios/PlayMatch.Core.AssemblyInfo.cs
+++ PlayMatch.Core/obj/Debug/net8.0-ios/PlayMatch.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlayMatch.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3793e07fe459f1303285c059cc974283b098a1cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+836f2793b262afb163162239ba55a437380d6d14")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlayMatch.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlayMatch.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
