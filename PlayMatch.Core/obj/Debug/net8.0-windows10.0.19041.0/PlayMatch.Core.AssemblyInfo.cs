b6f3df1c675444a6e49206c7ac76a7a032074c34
Henrique Ameixa <henrique.ameixa@prosperi.com.br>
2025-02-28 08:48:17 -0300

    .

--- PlayMatch.Core/obj/Debug/net8.0-windows10.0.19041.0/PlayMatch.Core.AssemblyInfo.cs
+++ PlayMatch.Core/obj/Debug/net8.0-windows10.0.19041.0/PlayMatch.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlayMatch.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fe9cf4ed06d313c53e4f20695d17c06327d41b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80cf01a69ef765152a14ad5942778f2f77232d7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlayMatch.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlayMatch.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
