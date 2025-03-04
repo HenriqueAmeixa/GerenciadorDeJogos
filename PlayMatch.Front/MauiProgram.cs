dc83988d7748db0c3e29f4cbab1bcb3a577ed986
henrique ameixa <henrique-ameixa@hotmail.com>
2025-03-04 14:23:52 -0300

    Corrigido erros do db no projeto novo

--- PlayMatch.Front/MauiProgram.cs
+++ PlayMatch.Front/MauiProgram.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.Extensions.Logging;
+using PlayMatch.Front.Services;
+using PlayMatch.Core.Data;
+using PlayMatch.Front.Mappers;
+using PlayMatch.Front.Components.Pages;
 
 namespace PlayMatch.Front
 {
@@ -13,6 +17,8 @@ namespace PlayMatch.Front
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 });
+            string dbPath = Path.Combine(FileSystem.AppDataDirectory, "playmatch.db3");
+
 
             builder.Services.AddMauiBlazorWebView();
 
@@ -20,7 +26,16 @@ namespace PlayMatch.Front
     		builder.Services.AddBlazorWebViewDeveloperTools();
     		builder.Logging.AddDebug();
 #endif
+            builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
+            builder.Services.AddSingleton<TimeService>();
 
+            builder.Services.AddSingleton<JogadorService>();
+            builder.Services.AddSingleton<TimeService>();
+            builder.Services.AddSingleton<PartidaService>();
+            builder.Services.AddSingleton<GolService>();
+            builder.Services.AddSingleton<Jogadores>();
+            builder.Services.AddSingleton(new PlayMatchDbContext(dbPath));
+            builder.Services.AddSingleton(typeof(IGenericRepository<>), typeof(SQLiteRepository<>));
             return builder.Build();
         }
     }
