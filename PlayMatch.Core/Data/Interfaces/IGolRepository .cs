ddffbc836a878e3360748bfe9c454d0d86c43aa2
henrique ameixa <henrique-ameixa@hotmail.com>
2025-03-04 07:58:00 -0300

    Atualizando .gitignore

--- PlayMatch.Core/Data/Interfaces/IGolRepository .cs
+++
@@ -1,9 +0,0 @@
-﻿using PlayMatch.Core.Models;
-
-namespace PlayMatch.Core.Data.Interfaces
-{
-    public interface IGolRepository : IGenericRepository<Gol>
-    {
-        Task<List<Gol>> GetByPartidaIdAsync(int partidaId);
-    }
-}
