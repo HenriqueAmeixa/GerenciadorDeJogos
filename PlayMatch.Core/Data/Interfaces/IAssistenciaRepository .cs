b6f84a6c571eebc2178b7ba0afd112a0d85cdc9d
henrique ameixa <henrique-ameixa@hotmail.com>
2025-03-05 08:25:06 -0300

    .

--- PlayMatch.Core/Data/Interfaces/IAssistenciaRepository .cs
+++
@@ -1,9 +0,0 @@
-﻿using PlayMatch.Core.Models;
-
-namespace PlayMatch.Core.Data.Interfaces
-{
-    public interface IAssistenciaRepository : IGenericRepository<Assistencia>
-    {
-        Task<List<Assistencia>> GetByPartidaIdAsync(int partidaId);
-    }
-}
--- PlayMatch.Core/Platforms/Tizen/PlatformClass1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace PlayMatch.Core
-{
-    // All the code in this file is only included on Tizen.
-    public class PlatformClass1
-    {
-    }
-}
